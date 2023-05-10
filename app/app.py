8b0c30715b0befb55075b3ce05c6246309861243
tizianoadv <tiziano.nardone.advance@gmail.com>
2023-05-10 16:28:45 +0000

    test done

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
 from flask import Flask, request, jsonify, render_template
+from datetime import datetime, timedelta
 import redis, os
+import pytz
+
 
 class MonitoringApp:
+
+
     def __init__(self):
+        """
+        This code initializes the MonitoringApp class 
+        and creates an instance of the Flask class named app
+        """
         self.app = Flask(__name__)
 
         # Connect to Redis database
@@ -13,39 +22,158 @@ class MonitoringApp:
 
         try:
             self.redis_conn = redis.Redis(host=redis_host, port=redis_port, password=redis_password, db=redis_db)
+            print("Connection to Redis database Sucessfull")
         except redis.exceptions.ConnectionError as e:
-            print("Redis connection error:", e)
+            print("Error connection to Redis database", e)
 
-        self.app.route('/flush', methods=['GET'])(self.flush_db)
+        # Set timezone GMT+2
+        self.timezone = pytz.timezone('Europe/Paris')
+
+        # Routes
+        self.app.route('/', methods=['GET'])(self.get_all_data)
+        self.app.route('/data/<id>', methods=['GET'])(self.get_data_by_id)
+        self.app.route('/data/hour', methods=['GET'])(self.get_data_last_hour)
+        self.app.route('/data/day', methods=['GET'])(self.get_data_last_day)
+        self.app.route('/data/week', methods=['GET'])(self.get_data_last_week)
+        self.app.route('/data/month', methods=['GET'])(self.get_data_last_month)
+        self.app.route('/data/year', methods=['GET'])(self.get_data_last_year)
         self.app.route('/data', methods=['POST'])(self.post_data)
-        self.app.route('/data', methods=['GET'])(self.get_data)
+        self.app.route('/flush', methods=['GET'])(self.flush_database)
 
-    def flush_db(self):
-        print("Flushing database")
-        self.redis_conn.flushdb()
-        print("Database flushed")
-        return jsonify({'message': 'DB flush successfully.'}), 200
 
-    def post_data(self):
-        payload = request.json
-        data = {
-            'temperature': str(payload['temperature']),
-            'humidity': str(payload['humidity']),
-            'luminosity': str(payload['luminosity']),
-            'timestamp': str(payload['timestamp'])
-        }
-        self.redis_conn.hmset(data['timestamp'], data)
-        return jsonify({'message': 'Data stored successfully.'}), 200   
+    def get_all_data(self):
+        """
+        Retrieves all data from the Redis database, 
+        sorts it in descending order by key and renders it in an HTML template
+        """
+        try:
+            data = {}
+            for key in self.redis_conn.scan_iter():
+                key_str = key.decode()
+                data[key_str] = {k.decode(): v.decode() for k, v in self.redis_conn.hgetall(key).items()}
+            sorted_data = dict(sorted(data.items(), reverse=True))
+            num_records = len(sorted_data)
+            return render_template('index.html', data=sorted_data, num_records=num_records, error_code=200)
+        except Exception as e:
+            error_msg = f"An error occurred while fetching data from database: {str(e)}"
+            return render_template('index.html', error=error_msg, error_code=500)
+ 
 
-    def get_data(self):
+    def get_data_by_id(self, id):
+        """
+        This function retrieves data from Redis database by its ID and stores it in a dictionary
+        It then returns a template that displays the retrieved data
+        """
         data = {}
+        if self.redis_conn.exists(id):
+            data[id] = {k.decode(): v.decode() for k, v in self.redis_conn.hgetall(id).items()}
+        return render_template('index.html', data=data)
+    
+
+    def get_data_last_hour(self):
+        """
+        This function retrieves data from Redis database that was added within the last hour
+        It then returns a template that displays the retrieved data
+        """
+        sorted_data = self.get_data_by_time_delta(hours=1)
+        return render_template('index.html', data=sorted_data)
+
+
+    def get_data_last_day(self):
+        """
+        This function retrieves data from Redis database that was added within the last 24 hours
+        It then returns a template that displays the retrieved data
+        """
+        sorted_data = self.get_data_by_time_delta(hours=24)
+        return render_template('index.html', data=sorted_data)
+    
+
+    def get_data_last_week(self):
+        """
+        This function retrieves data from Redis database that was added within the last week
+        It then returns a template that displays the retrieved data
+        """
+        sorted_data = self.get_data_by_time_delta(weeks=1)
+        return render_template('index.html', data=sorted_data)
+    
+
+    def get_data_last_month(self):
+        """
+        This function retrieves data from Redis database that was added within the last month
+        It then returns a template that displays the retrieved data
+        """
+        sorted_data = self.get_data_by_time_delta(months=1)
+        return render_template('index.html', data=sorted_data)
+    
+
+    def get_data_last_year(self):
+        """
+        This function retrieves data from Redis database that was added within the last year
+        It then returns a template that displays the retrieved data
+        """
+        sorted_data = self.get_data_by_time_delta(months=12)
+        return render_template('index.html', data=sorted_data)
+    
+
+    def get_data_by_time_delta(self, hours=0, weeks=0, months=0):
+        """This function retrieves data from Redis database within a specified time delta 
+        (hours, weeks, months or year) based on the timestamp associated with each key in the database
+         It returns the sorted data in reverse order
+         """
+        data = {}
+        now_str = datetime.now(self.timezone).strftime('%d-%m-%Y %H:%M:%S')
+        now_obj = datetime.strptime(now_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=self.timezone)
+        delta = timedelta(hours=hours, weeks=weeks, days=30*months)
+        timestamp_threshold = (now_obj - delta)
+
         for key in self.redis_conn.scan_iter():
             key_str = key.decode()
-            data[key_str] = {k.decode(): v.decode() for k, v in self.redis_conn.hgetall(key).items()}
-        return render_template('index.html', data=data)
+            timestamp_str = self.redis_conn.hget(key, 'timestamp').decode()
+            timestamp_obj = datetime.strptime(timestamp_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=self.timezone)
+            if timestamp_obj >= timestamp_threshold and timestamp_obj <= now_obj:
+                data[key_str] = {k.decode(): v.decode() for k, v in self.redis_conn.hgetall(key).items()}
+
+        sorted_data = dict(sorted(data.items(), reverse=True))
+        return sorted_data
+
+
+        
+    def post_data(self):
+        """
+        This function receives a JSON payload, extracts data from it
+        generates a new unique key for Redis
+        and stores the data in Redis with the new key
+        """
+        payload = request.json
+        data = {
+            'temperature': payload['temperature'],
+            'luminosity': payload['luminosity'],
+            'humidity': payload['humidity'],
+            'timestamp': payload['timestamp']
+        }
+        keys = self.redis_conn.keys()
+        if len(keys) > 0:
+            max_key = max(map(int, keys))
+            new_key = max_key + 1
+        else:
+            new_key = 1
+        self.redis_conn.hmset(str(new_key), data)
+        data['id'] = str(new_key)
+        return jsonify(data), 200
+
+
+    def flush_database(self):
+        try:
+            self.redis_conn.flushdb()
+            return jsonify({'message': 'DB flush successfully.'}), 200
+        except Exception as e:
+            error_msg = f"Failed to flush DB: {str(e)}"
+            return jsonify({'message': error_msg}), 500
+
 
     def run(self):
         self.app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 if __name__ == '__main__':
     monitoring_app = MonitoringApp()
--- app/test_app.py
+++ app/test_app.py
@@ -1,33 +1,273 @@
-import json
-import pytest
-from app import MonitoringApp
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import requests
+import pytz
 
-@pytest.fixture
-def app():
-    yield MonitoringApp().app
 
-@pytest.fixture
-def client(app):
-    with app.test_client() as client:
-        yield client
+ENDPOINT = "http://localhost:5000"
+TIMEZONE = pytz.timezone('Europe/Paris')
 
-def test_flush_db(client):
-    response = client.get('/flush')
+
+def test_can_call_endpoint():
+    """Test if the API endpoint can be called"""
+
+    response = requests.get(ENDPOINT + "/")
     assert response.status_code == 200
-    assert response.json == {'message': 'DB flush successfully.'}
 
-# def test_post_data(client):
-#     data = {
-#         'temperature': 20,
-#         'humidity': 60,
-#         'luminosity': 500,
-#         'timestamp': '2022-01-01T00:00:00Z'
-#     }
-#     response = client.post('/data', json=data)
-#     assert response.status_code == 200
-#     assert response.json == {'message': 'Data stored successfully.'}
 
-# def test_get_data(client):
-#     response = client.get('/data')
-#     assert response.status_code == 200
-#     assert isinstance(response.data, bytes)
+def test_flush_database():
+    """Test if the API can flush the database"""
+
+    response = requests.get(ENDPOINT + "/flush")
+    assert response.status_code == 200
+
+
+def test_get_all_data_format():
+    """Test if the API can get all data in a specific format"""
+
+    response = requests.get(ENDPOINT + "/")
+    data = parse_html_row(response.text)
+    # Check if all values are strings
+    assert all(isinstance(value, str) for row in data for value in row.values())
+    # Check if only valid keys
+    assert all(set(row.keys()) == {'id', 'temperature', 'luminosity', 'humidity', 'timestamp'} for row in data)
+
+
+def test_post_new_data():
+    """Test if the API can post new data and retrieve it"""
+
+    now = datetime.now(TIMEZONE)
+    post_data_response = send_post_data(now)
+    assert post_data_response.status_code == 200
+    post_data_response = post_data_response.json()
+    data_id = post_data_response["id"]
+
+    get_data_by_id_response = requests.get(ENDPOINT + f"/data/{data_id}")
+    assert get_data_by_id_response.status_code == 200
+
+    get_data_by_id_response = parse_html_row(get_data_by_id_response.text)
+    get_data_by_id_response = get_data_by_id_response[0]
+    assert get_data_by_id_response['id']            == post_data_response['id']
+    assert get_data_by_id_response['temperature']   == post_data_response['temperature']
+    assert get_data_by_id_response['humidity']      == post_data_response['humidity']
+    assert get_data_by_id_response['luminosity']    == post_data_response['luminosity']
+    assert get_data_by_id_response['timestamp']     == post_data_response['timestamp']
+
+
+def test_get_data_last_hour():
+    """Test if the API can get all data registered within the last hour"""
+
+    assert flush_database()
+    now = datetime.now(TIMEZONE)
+    # Generate data older than 1 hour
+    old_data = [ (now - timedelta(days=2)),(now - timedelta(hours=2))]
+    for timestamp in old_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    # Generate data within the last hour
+    new_data = [(now - timedelta(minutes=30)), (now) ]
+    for timestamp in new_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    
+    response = requests.get(ENDPOINT + "/data/hour")
+    assert response.status_code == 200
+    data = parse_html_row(response.text)
+    assert len(data) == 2  # Only new data should be returned
+
+    now_str = datetime.now(TIMEZONE).strftime('%d-%m-%Y %H:%M:%S')
+    now_obj = datetime.strptime(now_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+    timestamp_threshold = (now_obj - timedelta(hours=1))
+    for entry in data:
+        entry['timestamp']
+        timestamp_str = entry['timestamp']
+        timestamp_obj = datetime.strptime(timestamp_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+        assert timestamp_obj >= timestamp_threshold and timestamp_obj <= now_obj
+
+
+def test_get_data_last_day():
+    """Test if the API can get all data registered within the last day"""
+
+    assert flush_database()
+    now = datetime.now(TIMEZONE)
+    # Generate data older than 1 day
+    old_data = [ (now - timedelta(days=5)),(now - timedelta(days=3))]
+    for timestamp in old_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    # Generate data within the last hour
+    new_data = [(now - timedelta(hours=23)), (now - timedelta(hours=1)) ]
+    for timestamp in new_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    
+    response = requests.get(ENDPOINT + "/data/day")
+    assert response.status_code == 200
+    data = parse_html_row(response.text)
+    assert len(data) == 2  # Only new data should be returned
+
+    now_str = datetime.now(TIMEZONE).strftime('%d-%m-%Y %H:%M:%S')
+    now_obj = datetime.strptime(now_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+    timestamp_threshold = (now_obj - timedelta(hours=24))
+
+    for entry in data:
+        entry['timestamp']
+        timestamp_str = entry['timestamp']
+        timestamp_obj = datetime.strptime(timestamp_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+        assert timestamp_obj >= timestamp_threshold and timestamp_obj <= now_obj
+
+
+def test_get_data_last_week():
+    """Test if the API can get all data registered within the last week"""
+
+    assert flush_database()
+    now = datetime.now(TIMEZONE)
+
+    # Generate data older than 1 week
+    old_data = [ (now - timedelta(weeks=3)),(now - timedelta(weeks=2))]
+    for timestamp in old_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    # Generate data within the last week
+    new_data = [(now - timedelta(days=6)), (now - timedelta(hours=2)) ]
+    for timestamp in new_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    
+    response = requests.get(ENDPOINT + "/data/week")
+    assert response.status_code == 200
+    data = parse_html_row(response.text)
+    assert len(data) == 2  # Only new data should be returned
+
+    now_str = datetime.now(TIMEZONE).strftime('%d-%m-%Y %H:%M:%S')
+    now_obj = datetime.strptime(now_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+    timestamp_threshold = (now_obj - timedelta(weeks=1))
+
+    for entry in data:
+        entry['timestamp']
+        timestamp_str = entry['timestamp']
+        timestamp_obj = datetime.strptime(timestamp_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+        assert timestamp_obj >= timestamp_threshold and timestamp_obj <= now_obj
+
+
+def test_get_data_last_month():
+    """Test if the API can get all data registered within the last month"""
+
+    assert flush_database()
+    now = datetime.now(TIMEZONE)
+    # Generate data older than 1 month
+    old_data = [ (now - timedelta(weeks=10)),(now - timedelta(weeks=5))]
+    for timestamp in old_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    # Generate data within the last month
+    new_data = [(now - timedelta(weeks=3)), (now - timedelta(hours=2)) ]
+    for timestamp in new_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    
+    response = requests.get(ENDPOINT + "/data/month")
+    assert response.status_code == 200
+    data = parse_html_row(response.text)
+    assert len(data) == 2  # Only new data should be returned
+
+    now_str = datetime.now(TIMEZONE).strftime('%d-%m-%Y %H:%M:%S')
+    now_obj = datetime.strptime(now_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+    timestamp_threshold = (now_obj - timedelta(weeks=4))
+
+    for entry in data:
+        entry['timestamp']
+        timestamp_str = entry['timestamp']
+        timestamp_obj = datetime.strptime(timestamp_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+        assert timestamp_obj >= timestamp_threshold and timestamp_obj <= now_obj
+
+
+def test_get_data_last_year():
+    """Test if the API can get all data registered within the last year"""
+
+    assert flush_database()
+    now = datetime.now(TIMEZONE)
+    # Generate data older than 1 year
+    old_data = [ (now - timedelta(weeks=4*20)),(now - timedelta(weeks=4*13))]
+    for timestamp in old_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    # Generate data within the last year
+    new_data = [(now - timedelta(weeks=4*12)), (now - timedelta(hours=2)) ]
+    for timestamp in new_data:
+        response = send_post_data(timestamp)
+        assert response.status_code == 200
+    
+    response = requests.get(ENDPOINT + "/data/year")
+    assert response.status_code == 200
+    data = parse_html_row(response.text)
+    assert len(data) == 2  # Only new data should be returned
+
+    now_str = datetime.now(TIMEZONE).strftime('%d-%m-%Y %H:%M:%S')
+    now_obj = datetime.strptime(now_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+    timestamp_threshold = (now_obj - timedelta(weeks=4*12))
+
+    for entry in data:
+        entry['timestamp']
+        timestamp_str = entry['timestamp']
+        timestamp_obj = datetime.strptime(timestamp_str, '%d-%m-%Y %H:%M:%S').replace(tzinfo=TIMEZONE)
+        assert timestamp_obj >= timestamp_threshold and timestamp_obj <= now_obj
+
+
+
+
+def send_post_data(timestamp):
+    """Send Post data to the API with a specific time"""
+    payload = {
+        "temperature": str(20.0), 
+        "luminosity": str(30.0),
+        "humidity": str(40.0),
+        "timestamp": timestamp.strftime("%d-%m-%Y %H:%M:%S")
+    }
+    return requests.post(ENDPOINT + "/data", json=payload)
+
+
+def flush_database():
+    """Flush the database"""
+    return requests.get(ENDPOINT + "/flush")
+
+
+def parse_html_row(html):
+    """Parse the HTML and retrieve a list of dict that contains 
+    id, temp, lum, hum, timestamp"""
+
+    soup = BeautifulSoup(html, 'html.parser')
+    rows = soup.findAll('tr')
+    data = list()
+    id_data = list()
+    temperature_data = list()
+    luminosity_data = list()
+    humidity_data = list()
+    timestamp_data = list()
+    for row in rows:
+        td_tags = [tag.text for tag in row.findAll('td')]
+        i=0
+        for tag in td_tags:
+            if i == 0:
+                id_data.append(tag)
+            if i == 1:
+                temperature_data.append(tag)
+            if i == 2:
+                luminosity_data.append(tag)
+            if i == 3:
+                humidity_data.append(tag)
+            if i == 4:
+                timestamp_data.append(tag)
+            i+=1
+        
+    i=0
+    while i < len(id_data):
+        data.append({
+            'id': id_data[i],
+            'temperature': temperature_data[i],
+            'luminosity': luminosity_data[i],
+            'humidity': humidity_data[i],
+            'timestamp': timestamp_data[i]
+        })
+        i+=1
+    return data
