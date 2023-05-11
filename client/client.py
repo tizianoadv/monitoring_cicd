a5c80043c7ab848e6e3561377afde1c72609cf82
tizianoadv <tiziano.nardone.advance@gmail.com>
2023-05-11 08:52:14 +0000

    client

---
+++ client/client.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import requests
+import json
+import time
+import random
+import pytz
+
+APP_NAME = "monitoring_cicd-app-1"
+APP_ENDPOINT= f"http://{APP_NAME}:5000/data"
+TIMEZONE = pytz.timezone('Europe/Paris')
+
+while True:
+    temperature = round(random.uniform(10, 40), 2)
+    humidity = round(random.uniform(20, 80), 2)
+    luminosity = round(random.uniform(0, 100), 2)
+    timestamp_str = datetime.now(TIMEZONE).strftime('%d-%m-%Y %H:%M:%S')
+  
+    data = {
+        "temperature": str(temperature),
+        "humidity": str(humidity),
+        "luminosity": str(luminosity),
+        "timestamp": timestamp_str
+    }
+    
+    headers = {'Content-type': 'application/json'}
+    r = requests.post(APP_ENDPOINT, data=json.dumps(data), headers=headers)
+    print("Data sent to serverless function:", r.text)
+    
+    time.sleep(30)
